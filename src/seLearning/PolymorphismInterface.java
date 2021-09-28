ea0c76f76e8e3dba748fb30213eb33cb14e0a214
nee-yang <63096968+nee-yang@users.noreply.github.com>
2021-09-28 21:23:38 +0800

    多态的两种方式：接口与继承

---
+++ src/seLearning/PolymorphismInterface.java
@@ -0,0 +1,60 @@
+package seLearning;
+
+
+interface Car {
+    String getName();
+    int getPrice();
+}
+
+// 宝马
+class BMW implements Car {
+
+    @Override
+    public String getName() {
+        return "baoma";
+    }
+
+    @Override
+    public int getPrice() {
+        return 300000;
+    }
+}
+
+// 奇瑞qq
+class CherryQQ implements Car {
+    @Override
+    public String getName() {
+        return "qq";
+    }
+
+    @Override
+    public int getPrice() {
+        return 40000;
+    }
+}
+
+// 汽车出售店
+class CarShop {
+    private int sellMoney;
+
+    public void sellCar(Car car) {
+        System.out.println("type: " + car.getName() + "price: " + car.getPrice());
+        sellMoney += car.getPrice();
+    }
+
+    public int getSellMoney() {
+        return sellMoney;
+    }
+}
+
+public class PolymorphismInterface {
+    public static void main(String[] args) {
+
+        CarShop carShop = new CarShop();
+        Car BMW = new BMW();
+        Car cherry = new CherryQQ();
+        carShop.sellCar(BMW);
+        carShop.sellCar(cherry);
+        System.out.println("all sell money: " + carShop.getSellMoney());
+    }
+}
--- src/seLearning/PolymorphismLearning.java
+++ src/seLearning/PolymorphismLearning.java
@@ -1,10 +1,112 @@
 package seLearning;
 
 
+class Wine {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Wine(){
+    }
+
+    public String drink(){
+        return "喝的是 " + getName();
+    }
+
+    /**
+     * 重写toString()
+     */
+    public String toString(){
+        return null;
+    }
+
+    public void fun1(){
+        System.out.println("Wine 的Fun.....");
+        fun2();
+    }
+
+    public void fun2(){
+        System.out.println("Wine 的Fun2...");
+    }
+}
+
+class JNC extends Wine {
+
+    public JNC(){
+        setName("JNC");
+    }
+
+    /**
+     * 重写父类方法，实现多态
+     */
+    public String drink(){
+        return "喝的是 " + getName();
+    }
+
+    /**
+     * 重写toString()
+     */
+    public String toString(){
+        return "Wine : " + getName();
+    }
+
+    public void fun1(String a){
+        System.out.println("JNC 的 Fun1...");
+        fun2();
+    }
+
+    /**
+     * 子类重写父类方法
+     * 指向子类的父类引用调用fun2时，必定是调用该方法
+     */
+    public void fun2(){
+        System.out.println("JNC 的Fun2...");
+    }
+}
+
+
+class JGJ extends Wine{
+    public JGJ(){
+        setName("JGJ");
+    }
+
+    /**
+     * 重写父类方法，实现多态
+     */
+    public String drink(){
+        return "喝的是 " + getName();
+    }
+
+    /**
+     * 重写toString()
+     */
+    public String toString(){
+        return "Wine : " + getName();
+    }
+}
+
 
 public class PolymorphismLearning {
 
     public static void main(String[] args) {
+//        Wine a = new JNC();
+//        a.fun1();
+        Wine[] wines = new Wine[2];
+        JNC jnc = new JNC();
+        JGJ jgj = new JGJ();
 
+        wines[0] = jnc;
+        wines[1] = jgj;
+
+        for (Wine wine:wines) {
+            System.out.println(wine.toString() + "--" + wine.drink());
+        }
     }
 }
