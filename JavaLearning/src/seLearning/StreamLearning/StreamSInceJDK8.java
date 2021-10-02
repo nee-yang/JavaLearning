6096d5c22c9230a0bf11610dc70d40d83f25fa6f
nee-yang <63096968+nee-yang@users.noreply.github.com>
2021-10-02 15:23:36 +0800

    Stream流的使用

---
+++ JavaLearning/src/seLearning/StreamLearning/StreamSInceJDK8.java
@@ -0,0 +1,19 @@
+package seLearning.StreamLearning;
+
+/**
+ *Stream是java8的新功能，极大简化了数据的处理
+ *      我们可以使用 java.util.Stream 对一个包含一个或多个元素的集合做各种操作。
+ *      这些操作可能是 中间操作 亦或是 终端操作。 终端操作会返回一个结果，而中间操作会返回一个 Stream 流。
+ * PS：Map 不支持 Stream 流，Stream 流支持同步执行，也支持并发执行。
+ *
+ *
+ * */
+
+
+
+public class StreamSInceJDK8 {
+    public static void main(String[] args) {
+
+        System.out.println("dff" + 1);
+    }
+}
