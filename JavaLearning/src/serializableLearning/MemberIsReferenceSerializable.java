213bcb3f5c0019ba5c4da9ca97f0c7ffd4c63a09
nee-yang <63096968+nee-yang@users.noreply.github.com>
2021-10-04 12:46:25 +0800

    serializable

---
+++ JavaLearning/src/serializableLearning/MemberIsReferenceSerializable.java
@@ -0,0 +1,52 @@
+package serializableLearning;
+
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+/** [2] 成员是引用的序列化
+ *  如果一个可序列化的类的成员既不是基本类型，也不是string类型，那么这个引用类型也必须是可序列化的，否则会导致该类不能序列化
+ *  该程序会报错 ：java.io.NotSerializableException: seLearning.serializableLearning.People
+ */
+
+
+class People {
+    private String name;
+    private int age;
+
+    public People(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
+
+public class MemberIsReferenceSerializable implements Serializable {
+
+    private String name;
+    private People people;
+
+    public MemberIsReferenceSerializable(String name, People people) {
+        this.name = name;
+        this.people = people;
+    }
+
+    public static void main(String[] args) {
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("member.txt"));
+            People people = new People("hh", 0);
+            oos.writeObject(people);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ JavaLearning/src/serializableLearning/Teacher.java
@@ -0,0 +1,38 @@
+package serializableLearning;
+
+import java.io.Serializable;
+
+public class Teacher implements Serializable {
+
+    private String name;
+    private Person person;
+
+    public Teacher(String name, Person person) {
+        this.name = name;
+        this.person = person;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "name='" + name + '\'' +
+                ", person=" + person +
+                '}';
+    }
+}
